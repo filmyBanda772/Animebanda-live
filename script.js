21b3fef460a22dfbaa9e3e4fa3451dd6d10c9211
filmybanda <www.sumitkushwaha333@gmail.com>
2026-09-19 14:39:16 +0530

    Initial setup for AnimeBanda

---
+++ script.js
@@ -0,0 +1,61 @@
+    <!-- ================= JAVASCRIPT ANIMATION ================= -->
+        document.addEventListener("DOMContentLoaded", function() {
+            const cards = document.querySelectorAll('.movie-card');
+            
+            // Intersection Observer (check that card is shown in screen or not)
+            const observer = new IntersectionObserver((entries) => {
+                let delay = 0;
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        // single card (150ms gap) load karne ka logic
+                        setTimeout(() => {
+                            entry.target.classList.add('show');
+                        }, delay);
+                        delay += 150; // next card ke liye 150ms extra
+                        observer.unobserve(entry.target); // single time load 
+                    }
+                });
+            }, { threshold: 0.1 }); // card load when 10% is shown 
+
+            //  (Observer) for all cards
+            cards.forEach(card => observer.observe(card));
+        });
+    // ==========================================
+    // 3. SEARCH BAR LOGIC (On Enter Press)
+    // ==========================================
+    const searchInput = document.getElementById('searchInput');
+    const noResultMsg = document.getElementById('noResultMsg');
+    
+    if (searchInput) {
+        // 'keyup' ki jagah 'keydown' lagaya hai aur Enter key check kar rahe hain
+        searchInput.addEventListener('keydown', function(event) {
+            
+            // Jab user 'Enter' dabayega tabhi code chalega
+            if (event.key === 'Enter') {
+                event.preventDefault(); // Default behavior roko
+                
+                const filter = searchInput.value.toLowerCase();
+                const cards = document.querySelectorAll('.movie-card');
+                let matchCount = 0; // Ginenge kitne card match hue
+                
+                cards.forEach(card => {
+                    const title = card.querySelector('.movie-title').innerText.toLowerCase();
+                    if (title.includes(filter)) {
+                        card.style.display = ""; // Match hua to dikhao
+                        matchCount++; // Count badhao
+                    } else {
+                        card.style.display = "none"; // Match nahi hua to chupao
+                    }
+                });
+
+                // Agar ek bhi card match nahi hua to "No result Found!" dikhao
+                if (noResultMsg) {
+                    if (matchCount === 0) {
+                        noResultMsg.style.display = "block";
+                    } else {
+                        noResultMsg.style.display = "none";
+                    }
+                }
+            }
+        });
+    }
